fd39802cbd9cb364e56febc94e1cca1c6c3f09bb
Gil Emerson <emerson-gil@outlook.com>
2016-02-22 20:02:10 -0500

    Program Complete: Examples of get & set methods, constructors and public & private variables.

--- EclipseProjectTwo/src/Program.java
+++ EclipseProjectTwo/src/Program.java
@@ -1,9 +1,20 @@
 
 public class Program {
+	
+public static Person person; //Reference to the  'Person Class'
+private String _name = "";
 
 	public static void main(String[] args) {
-		System.out.println("Program Started..");
+				
+		System.out.println("Program Started..");	
 
+		person = new Person(22);
+		person.set_name("Gil");
+		
+		System.out.println(person.get_name());
+		
+		person.runs(); //printing public method to the console
+		
 	}
 
 }
